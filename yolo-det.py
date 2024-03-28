3e25755b0030eb005be004c0f428ee0feb264938
rhog23 <raymonde.saragih@gmail.com>
2024-03-28 22:43:49 +0700

    added real-time detection and initial tracking app

---
+++ yolo-det.py
@@ -0,0 +1,43 @@
+from ultralytics import YOLO
+import matplotlib.pyplot as plt
+import cv2
+
+model = YOLO("./models/yolov8n.pt", task="detect")
+
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 224)
+cap.set(4, 224)
+
+while True:
+    success, frame = cap.read()
+
+    results = model(frame, imgsz=224)
+
+    for result in results:
+        box = result.boxes
+        coords = box.xyxy
+        if len(coords) > 1:
+            x = int(coords[0][0])
+            y = int(coords[0][1])
+            w = int(coords[0][2])
+            h = int(coords[0][3])
+
+            cv2.rectangle(frame, (x, y), (w, h), (0, 255, 0), 1)
+            cv2.putText(
+                frame,
+                f"{model.names[int(box.cls[0])]}",
+                (x, y),
+                1,
+                1,
+                (0, 0, 0),
+                1,
+                cv2.FONT_HERSHEY_SIMPLEX,
+            )
+
+    cv2.imshow("detector", frame)
+    if cv2.waitKey(1) == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
