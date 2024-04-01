491ae1746055ba3bf129a8e64dd6f1264107848c
rhog23 <raymonde.saragih@gmail.com>
2024-04-01 19:54:31 +0700

    [info] 🟡 added example code for object counter from supervision. [todo] 🔵 create a custom object counter

--- app.py
+++ app.py
@@ -1,32 +1,52 @@
-import numpy as np
+import cv2, argparse
 import supervision as sv
 from ultralytics import YOLO
 
-model = YOLO("./models/yolov8m.pt", task="detect")
-tracker = sv.ByteTrack()
-box_annotator = sv.BoundingBoxAnnotator()
-label_annotator = sv.LabelAnnotator()
+
+def parse_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Class Monitoring")
+    parser.add_argument("--imgsz", default=192, type=int)
+    args = parser.parse_args()
+    return args
 
 
-def callback(frame: np.ndarray, _: int) -> np.ndarray:
-    results = model(frame)[0]
-    detections = sv.Detections.from_ultralytics(results)
-    detections = tracker.update_with_detections(detections)
+def main():
+    args = parse_arguments()
 
-    labels = [
-        f"#{tracker_id} {results.names[class_id]}"
-        for class_id, tracker_id in zip(detections.class_id, detections.tracker_id)
-    ]
+    cap = cv2.VideoCapture(0)
 
-    annotated_frame = box_annotator.annotate(frame.copy(), detections=detections)
+    while True:
+        _, frame = cap.read()
 
-    return label_annotator.annotate(
-        annotated_frame, detections=detections, labels=labels
-    )
+        results = model(frame, imgsz=args.imgsz)[0]
+        detections = sv.Detections.from_ultralytics(results)
+        detections = tracker.update_with_detections(detections)
+
+        labels = [
+            f"#{tracker_id} {results.names[class_id]}"
+            for class_id, tracker_id in zip(detections.class_id, detections.tracker_id)
+        ]
+
+        annotated_frame = box_annotator.annotate(frame.copy(), detections=detections)
+
+        cv2.imshow(
+            "class monitoring",
+            label_annotator.annotate(
+                annotated_frame, detections=detections, labels=labels
+            ),
+        )
+
+        if cv2.waitKey(1) == ord("q"):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
 
 
-sv.process_video(
-    source_path="./test-images/people-walking.mp4",
-    target_path="result.mp4",
-    callback=callback,
-)
+if __name__ == "__main__":
+    model = YOLO("./models/yolov8n.pt", task="detect")
+    tracker = sv.ByteTrack()
+    box_annotator = sv.BoundingBoxAnnotator()
+    label_annotator = sv.LabelAnnotator()
+
+    main()
